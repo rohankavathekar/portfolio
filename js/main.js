60589dd4cf3032229f723c771eba6e6c45d134ba
Rohan Kavathekar <rohan.kavathekar@wgblindia.com>
2020-07-05 16:31:26 +0530

    carousel removed

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@ $(document).ready(function(){
       this.changeLogo();
       this.rellax();
       this.workDetails();
-      this.carousel();
     },
     cursor: function() {
       $(document).mousemove(function(event){
@@ -52,27 +51,6 @@ $(document).ready(function(){
         $(".details").removeClass("hover");
   	  }
     );
-    },
-    carousel: function() {
-      $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: true,
-        navText: [
-          "<i class='fa fa-caret-left'></i>",
-          "<i class='fa fa-caret-right'></i>"
-        ],
-        autoplay: true,
-        autoplayHoverPause: true,
-        responsive: {
-          0: {
-            items: 1
-          },
-          1000: {
-            items: 3
-          }
-        }
-      })
     }
   }
   Main.init();
