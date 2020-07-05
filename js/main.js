e67aa94215ee8d5a3c1777c73c7b9bf83562872d
Rohan Kavathekar <rohan.kavathekar@wgblindia.com>
2020-07-05 16:09:28 +0530

    carousel added

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
       this.changeLogo();
       this.rellax();
       this.workDetails();
+      this.carousel();
     },
     cursor: function() {
       $(document).mousemove(function(event){
@@ -51,6 +52,27 @@ $(document).ready(function(){
         $(".details").removeClass("hover");
   	  }
     );
+    },
+    carousel: function() {
+      $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        navText: [
+          "<i class='fa fa-caret-left'></i>",
+          "<i class='fa fa-caret-right'></i>"
+        ],
+        autoplay: true,
+        autoplayHoverPause: true,
+        responsive: {
+          0: {
+            items: 1
+          },
+          1000: {
+            items: 3
+          }
+        }
+      })
     }
   }
   Main.init();
